363ce9ddd096cce21ef0da8add93ac3f8e4e0e98
H3nr1Yuk1 <mathsilvaesilva@gmail.com>
2023-05-29 11:21:51 -0300

    Alterações para o banco

--- src/Entidades/PastaDados.java
+++ src/Entidades/PastaDados.java
@@ -2,6 +2,7 @@ package Entidades;
 
 import java.util.ArrayList;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -12,6 +13,7 @@ public class PastaDados {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int id;
+	@Column(unique = true)
     private String nomePasta;
     private ArrayList<DadoCustomizado> dadosPasta;
 	
